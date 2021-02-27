375bbc0600b3d578e92137895023cd961cba984b
Josh Trivedi <josh.trivedi@gmail.com>
2021-02-28 01:17:30 +0530

    require()

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js')
 const bot = new Discord.Client()
 const tk = require('./config.json')
 const react = require('./reaction-roles')
+const polls = require('./polls')
 bot.on('ready', () =>{
     console.log('Board bot is online')
     react(bot)
