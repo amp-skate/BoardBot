8c35e14c01622de902f4bc14b28948060c72cb94
Josh Trivedi <josh.trivedi@gmail.com>
2021-02-27 23:17:29 +0530

    import reaction-roles

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 const tk = require('./config.json')
+const react = require('./reaction-roles')
 bot.on('ready', () =>{
     console.log('Board bot is online')
+    react(bot)
 })
 
 bot.on('message',msg=>{
