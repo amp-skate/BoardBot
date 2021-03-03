f898fc4008d41ab8068c8fe07b1545c04eaf3248
Josh Trivedi <josh.trivedi@gmail.com>
2021-03-04 00:11:12 +0530

    dist files add

--- commands/dist/testing.dev.js
+++ commands/dist/testing.dev.js
@@ -1,52 +1,62 @@
 "use strict";
 
-var _require = require('discord.js'),
-    Emoji = _require.Emoji,
-    Message = _require.Message;
+var Discord = require('discord.js');
 
-var reg = /[\uD83C][\uDDE6-\uDDFF][\uD83C][\uDDE6-\uDDFF]/;
+var _require = require('../config.json'),
+    moderator_roles = _require.moderator_roles,
+    profile_channel = _require.profile_channel,
+    testing_channels = _require.testing_channels;
 
-var country = require('../flag-emojis');
+function removeEmojis(string) {
+  var regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
+  return string.replace(regex, "");
+}
 
-var customRole;
-
-var _require2 = require('../config.json'),
-    polls_channel = _require2.polls_channel;
-
-function getCollectionFromEmoji(args) {
-  var matching = country.find(function (element) {
-    return element.emoji == args;
-  });
-  return matching.name;
+function isOnlyEmoji(string) {
+  return !removeEmojis(string);
 }
 
 module.exports = {
   name: "testing",
-  description: "testing code for the dev",
+  description: "testing function for the dev",
   execute: function execute(msg, args) {
-    var channel = msg.channel,
-        content = msg.content;
+    var sent1, sent2;
+    return regeneratorRuntime.async(function execute$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            /*let embed = new Discord.MessageEmbed()
+            .setTitle('Choose your profile!')
+            .setColor('GREEN')
+            .setDescription('React to the emojis to gain the roles!')
+            let msgEmbed = await msg.channel.send(embed)
+            msgEmbed.react('1️⃣')
+            msgEmbed.react('2️⃣') 
+            msgEmbed.react('3️⃣')*/
+            msg.channel.send("Hey there! Let's get to know you a little better. React to these posts to get these roles added to your profile:*");
+            _context.next = 3;
+            return regeneratorRuntime.awrap(msg.channel.send("**What pronouns do you go by?\n:one: He/Him\n:two: She/Her\n:three: They/Them**\n"));
 
-    if (!reg.test(args)) {
-      msg.channel.send('Please enter a valid country flag emoji like !flag 🇺🇲 , and enter one flag at a time only!');
-      return;
-    }
+          case 3:
+            sent1 = _context.sent;
+            sent.react('1️⃣');
+            sent.react('2️⃣');
+            sent.react('3️⃣');
+            msg.channel.send("What's your experience level in terms of longboarding?");
+            _context.next = 10;
+            return regeneratorRuntime.awrap(msg.channel.send("**What's your experiece in terms of longboarding?\n:baby: Beginner\n:child: Intermediate\n:person_curly_hair: Advanced\n:older_adult: Professional\n:mage: Wizard**"));
 
-    customRole = "I'm from ".concat(getCollectionFromEmoji(args), "!");
+          case 10:
+            sent2 = _context.sent;
+            sent2.react('👶');
+            sent2.react('🧒');
+            sent2.react(':person_curly_hair:');
 
-    try {
-      if (!msg.member.roles.cache.find(function (r) {
-        return r.name === customRole;
-      })) {
-        var rol = msg.member.guild.roles.cache.find(function (r) {
-          return r.name === customRole;
-        });
-        msg.guild.members.cache.get(msg.author.id).roles.add(rol);
-        msg.react('👍');
-      } else msg.channel.send("you already have the country assigned role!");
-    } catch (e) {
-      msg.channel.send('Something seems wrong, maybe your country tag is not available at the moment, please reach out to a moderator or an admin! Sorry for the inconvienience! ');
-      console.log(e);
-    }
+          case 14:
+          case "end":
+            return _context.stop();
+        }
+      }
+    });
   }
 };
