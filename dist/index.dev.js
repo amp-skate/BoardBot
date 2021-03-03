d123ad98e005d29ff2652cd22f13157e6dc19c15
Josh Trivedi <josh.trivedi@gmail.com>
2021-03-04 00:12:07 +0530

    dist file add

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -5,19 +5,21 @@ var _require = require('./config.json'),
     youtube_link = _require.youtube_link,
     moderator_roles = _require.moderator_roles,
     NOTHING_SPECIAL = _require.NOTHING_SPECIAL,
-    flag_drop = _require.flag_drop;
+    flag_drop = _require.flag_drop,
+    testing_channels = _require.testing_channels,
+    profile_channel = _require.profile_channel;
 
 var Discord = require('discord.js');
 
-var bot = new Discord.Client();
+var bot = new Discord.Client({
+  partials: ["MESSAGE", "CHANNEL", "REACTION"]
+});
 
 var polls = require('./polls');
 
-var fs = require('fs');
+var fs = require('fs'); //const MessageModel = require('./database/message');
+//const database = require('./database/database');
 
-var MessageModel = require('./database/message');
-
-var database = require('./database/database');
 
 var rolereaction = require('./commands/rolereaction');
 
@@ -61,12 +63,7 @@ try {
 
 bot.on('ready', function () {
   console.log('Board bot is online');
-  polls(bot);
-  database.then(function () {
-    return console.log('MongoDB is Connected!');
-  })["catch"](function (err) {
-    return console.log(err);
-  });
+  polls(bot); //database.then(()=>console.log('MongoDB is Connected!')).catch(err=>console.log(err))
 });
 bot.on('message', function (msg) {
   var args = msg.content.slice(prefix.length).split(/ +/);
@@ -76,20 +73,98 @@ bot.on('message', function (msg) {
     bot.commands.get('ping').execute(msg, args);
   } else if (command === "youtube" || command === "yt") {
     bot.commands.get('youtube').execute(msg, args, youtube_link);
-  } else if (flag_drop.includes(msg.channel.id)) {
+  } else if (flag_drop.includes(msg.channel.id) && !msg.author.bot) {
     flagDrop.execute(bot, msg);
   }
-  /*else if(command == "flag"){
-      bot.commands.get('testing').execute(msg,args)
-  }
-  else if(command === "getreaction"){
-      bot.commands.get('createrolereaction').execute(bot,msg,args)
-  }else if(command === "try"){
-      rolereaction:run(bot,msg,args)
-  }
-  else if(command == "reactionrole"){
-      bot.commands.get('reactionrole').execute(msg, args, Discord, client)
-  }*/
+});
+bot.on('messageReactionAdd', function _callee(reaction, user) {
+  var role, _role, _role2;
 
+  return regeneratorRuntime.async(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          if (!reaction.message.partial) {
+            _context.next = 3;
+            break;
+          }
+
+          _context.next = 3;
+          return regeneratorRuntime.awrap(reaction.message.fetch());
+
+        case 3:
+          if (!reaction.partial) {
+            _context.next = 6;
+            break;
+          }
+
+          _context.next = 6;
+          return regeneratorRuntime.awrap(reaction.fetch());
+
+        case 6:
+          if (!user.bot) {
+            _context.next = 8;
+            break;
+          }
+
+          return _context.abrupt("return");
+
+        case 8:
+          if (reaction.message.guild) {
+            _context.next = 10;
+            break;
+          }
+
+          return _context.abrupt("return");
+
+        case 10:
+          if (!profile_channel.includes(reaction.message.channel.id)) {
+            _context.next = 25;
+            break;
+          }
+
+          if (!(reaction.emoji.name === '1️⃣')) {
+            _context.next = 16;
+            break;
+          }
+
+          console.log('yes first given');
+          role = reaction.message.guild.roles.cache.find(function (r) {
+            return r.name === "he/him";
+          });
+          _context.next = 16;
+          return regeneratorRuntime.awrap(reaction.message.guild.members.cache.get(user.id).roles.add(role));
+
+        case 16:
+          if (!(reaction.emoji.name === '2️⃣')) {
+            _context.next = 21;
+            break;
+          }
+
+          console.log('yes second given');
+          _role = reaction.message.guild.roles.cache.find(function (r) {
+            return r.name.toLowerCase() === "she/her";
+          });
+          _context.next = 21;
+          return regeneratorRuntime.awrap(reaction.message.guild.members.cache.get(user.id).roles.add(_role));
+
+        case 21:
+          if (!(reaction.emoji.name === '3️⃣')) {
+            _context.next = 25;
+            break;
+          }
+
+          _role2 = reaction.message.guild.roles.cache.find(function (r) {
+            return r.name.toLowerCase() === "they/them";
+          });
+          _context.next = 25;
+          return regeneratorRuntime.awrap(reaction.message.guild.members.cache.get(user.id).roles.add(_role2));
+
+        case 25:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
 });
 bot.login(NOTHING_SPECIAL);
