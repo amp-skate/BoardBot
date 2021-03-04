c27bbed6167d00f07dc5a5b89a4ea28790ed835e
Josh Trivedi <50212531+joshtrivedi@users.noreply.github.com>
2021-03-04 13:41:38 +0530

    Delete database.dev.js

--- database/dist/database.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var mongoose = require('mongoose');
-
-module.exports = mongoose.connect("mongodb://127.0.0.1:27017/RoleReaction", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
