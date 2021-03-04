8c72ad06ac2f1da21c2937f2fb644c5348e457eb
Josh Trivedi <josh.trivedi@gmail.com>
2021-03-04 13:45:27 +0530

    purge unused imports

--- index.js
+++ index.js
@@ -2,38 +2,28 @@ const {
     prefix,
     youtube_link,
     moderator_roles,
-    NOTHING_SPECIAL,
     flag_drop,
-    testing_channels,
-    profile_channel
 } = require('./config.json')
 const Discord = require('discord.js')
 const bot = new Discord.Client({partials: ["MESSAGE","CHANNEL","REACTION"]})
 const polls = require('./polls')
 const fs = require('fs')
-//const MessageModel = require('./database/message');
-//const database = require('./database/database');
-const rolereaction = require('./commands/rolereaction')
-const { run } = require('./commands/rolereaction')
+
 const flagDrop = require('./flag-drop')
 const reactionAdd = require('./reaction-add')
 const reactionRemove = require('./reaction-remove')
-//const reactRole = require('./commands/react-role')
-//const { env } = require('process')
 
 bot.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
 for(const file of commandFiles){
     const command = require(`./commands/${file}`)
-
     bot.commands.set(command.name, command);
 }
 
 bot.on('ready', () =>{
     console.log('Board bot is online')
     polls(bot);
-    //database.then(()=>console.log('MongoDB is Connected!')).catch(err=>console.log(err))
     reactionAdd(bot);
     reactionRemove(bot);
 })
